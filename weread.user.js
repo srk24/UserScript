5514b657b9018da01e209b0cd66a681e72b155ac
Wayne <25836646+srk24@users.noreply.github.com>
2023-09-17 10:13:18 +0800

    Update weread.user.js

--- weread.user.js
+++ weread.user.js
@@ -7,7 +7,7 @@
 // @author      @todd
 // @description 2023/9/13 20:49:59
 // @homepageURL https://github.com/srk24/UserScript/blob/main/weread.user.js
-// @@downloadURL https://github.com/srk24/UserScript/raw/main/weread.user.js
+// @downloadURL https://github.com/srk24/UserScript/raw/main/weread.user.js
 // ==/UserScript==
 
 GM_addStyle("*{font-family: serif !important;}");
