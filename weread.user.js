a940f6121d511ac3232698b33c0258aa458effa9
Wayne <25836646+srk24@users.noreply.github.com>
2023-09-17 10:12:47 +0800

    update

--- weread.user.js
+++ weread.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// @name        微信读书字体修改
+// @name        微信读书修改为衬线字体
 // @description  微信读书使用浏览器默认衬线字体
 // @match       https://weread.qq.com/web/reader/*
 // @grant       GM_addStyle
 // @version     0.1.0
 // @author      @todd
 // @description 2023/9/13 20:49:59
+// @homepageURL https://github.com/srk24/UserScript/blob/main/weread.user.js
+// @@downloadURL https://github.com/srk24/UserScript/raw/main/weread.user.js
 // ==/UserScript==
 
 GM_addStyle("*{font-family: serif !important;}");
